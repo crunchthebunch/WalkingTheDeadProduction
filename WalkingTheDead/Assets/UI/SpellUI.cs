6c20e64541a0596f005ac0c94e64b9611004882d
Zsombor Pirok <zsombor.pirok@gmail.com>
2019-05-03 17:53:57 +1200

    Gameplay UI done, Custom Cursor under progress

---
+++ WalkingTheDead/Assets/UI/SpellUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellUI : MonoBehaviour
+{
+
+    [SerializeField] string spellDescription;
+
+    public string SpellDescription { get => spellDescription; set => spellDescription = value; }
+
+}
---
+++ WalkingTheDead/Assets/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject spellDescription;
+    [SerializeField] Texture2D cursorTexture;
+    CursorMode cursorMode = CursorMode.Auto;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, cursorMode);
+    }
+
+    public void ShowSpellDescription(SpellUI spell)
+    {
+        spellDescription.SetActive(true);
+
+        // Update the spell Text
+        spellDescription.GetComponentInChildren<TextMeshProUGUI>().text = spell.SpellDescription;
+    }
+
+    public void HideSpellDescription()
+    {
+        spellDescription.SetActive(false);
+    }
+
+    public void ShowStatValue(StatUI stat)
+    {
+        // Make the value text visible
+        stat.gameObject.SetActive(true);
+    }
+
+    public void HideStatValue(StatUI stat)
+    {
+        // Make value invisible
+        stat.gameObject.SetActive(false);
+    }
+}
