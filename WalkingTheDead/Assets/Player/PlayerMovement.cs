b10173f3f0fd429ca10c32523ea1f3e0a0adc5c7
Varun Naval <varun.naval@gmail.com>
2019-05-03 15:11:02 +1200

    Resurrection and Soul Collection Auras working!

--- WalkingTheDead/Assets/Player/PlayerMovement.cs
+++ WalkingTheDead/Assets/Player/PlayerMovement.cs
@@ -11,12 +11,19 @@ public class PlayerMovement : MonoBehaviour
 
     Quaternion tempQuaternion = Quaternion.Euler(30.0f, 0.0f, 30.0f);
 
+    public SphereCollider soulCollectionScanner;
+    public SphereCollider resurrectionScanner;
+    public float radiusIncrease;
 
+    bool soulCollectionActive;
+    bool resurrectionActive;
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
 
+        soulCollectionActive = false;
+        resurrectionActive = false;
     }
 
     // Update is called once per frame
@@ -35,7 +42,7 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
             tempQuaternion = transform.rotation;
         }
-       
+
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             anim.SetBool("isWalking", true);
@@ -46,10 +53,46 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
+        // Soul Collection Sphere
+        if (Input.GetKey("e") && !resurrectionActive && soulCollectionScanner.radius <= 10.0f)
+        {
+            soulCollectionScanner.radius += radiusIncrease;
+            soulCollectionActive = true;
+        }
+        else if (!Input.GetKey("e") && soulCollectionScanner.radius > 0.0f)
+        {
+            soulCollectionScanner.radius -= (radiusIncrease * 3);
+        }
+        else if (soulCollectionScanner.radius <= 0.0f)
+        {
+            soulCollectionActive = false;
+        }
+
+        // Resurrection Sphere
+        if (Input.GetKey("r") && !soulCollectionActive && resurrectionScanner.radius <= 10.0f)
+        {
+            resurrectionScanner.radius += radiusIncrease;
+            resurrectionActive = true;
+        }
+        else if (!Input.GetKey("r") && resurrectionScanner.radius > 0.0f)
+        {
+            resurrectionScanner.radius -= (radiusIncrease * 3);
+        }
+        else if (resurrectionScanner.radius <= 0.0f)
+        {
+            resurrectionActive = false;
+        }
+
+
 
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, soulCollectionScanner.radius);
 
-
-
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, resurrectionScanner.radius);
+    }
 }
