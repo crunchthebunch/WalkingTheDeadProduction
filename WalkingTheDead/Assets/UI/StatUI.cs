5c6814661e8285263a28e51aeb159a90e2299a31
Zsombor Pirok <zsombor.pirok@gmail.com>
2019-05-07 23:02:36 +1200

    Glow is now Triggering

--- WalkingTheDead/Assets/UI/StatUI.cs
+++ WalkingTheDead/Assets/UI/StatUI.cs
@@ -6,54 +6,36 @@ using UnityEngine.UI;
 public class StatUI : MonoBehaviour
 {
     Slider slider;
-    ParticleSystem glowEffect;
-    [SerializeField] float triggerValue = 0.3f;
+    ParticleSystem.EmissionModule emissionModule;
+    [SerializeField] float warningValue = 0.3f;
 
     // Start is called before the first frame update
     void Awake()
     {
         slider = GetComponent<Slider>();
-        glowEffect = GetComponentInChildren<ParticleSystem>();
-
-        glowEffect.Pause(); // TODO Fix
+        emissionModule = GetComponentInChildren<ParticleSystem>().emission;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetupStatUI(float warningValue, float startingValue, float maximumValue)
     {
-        // If the value is under the trigger value
-        if (slider.value / slider.maxValue < triggerValue)
-        {
-            
-            // Play Glow if it's not being played
-            if (!glowEffect.isPlaying)
-            {
-                print("Start Playing");
-                glowEffect.Play();
-            }
-        }
-        else
-        {
-            // Disable Emission
-            if (glowEffect.isPlaying) { glowEffect.Pause(); }
-        }
-    }
-
-    public void SetupStatUI(float currentValue, float maximumValue)
-    {
-        slider.value = currentValue;
-        slider.value = maximumValue;
+        this.warningValue = warningValue;
+        slider.value = startingValue;
+        slider.maxValue = maximumValue;
     }
 
     public void SetCurrentValue(float newValue)
     {
         slider.value = newValue;
 
-        //// If the value is under the trigger value
-        //if (slider.value / slider.maxValue < triggerValue)
-        //{
-        //    // Play Glow if it's not being played
-        //    if (!glowEffect.isPlaying) { glowEffect.Play(); }
-        //}
+        // If the value is under the trigger value
+        if (slider.value / slider.maxValue < warningValue)
+        {
+            emissionModule.enabled = true;
+        }
+        else
+        {
+            // Disable Emission
+            emissionModule.enabled = false;
+        }
     }
 }
